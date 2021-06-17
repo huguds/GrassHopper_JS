5e2ed6c3a46be16ce34f640e5e787ec770ae5f5a
huguds <vfvitinhoblack@gmail.com>
2021-06-17 19:26:42 -0300

    Exercicio de estrutura de repetição

---
+++ Exercicio02.js
@@ -0,0 +1,10 @@
+// Programador: Victor Hugo | Algoritmo: estrutura de decisão | JavaScript Data: 17/06/2021
+
+var nota1 = 0;
+var nota2 = 10;
+var media = (nota1+nota2)/2;
+if (media < 7){
+    console.log("O aluno está reprovado !");
+}else{
+    console.log("O aluno está aprovado !");
+}
