f7aa5721fae435be571193543a9c7c0fe08bd0ec
huguds <vfvitinhoblack@gmail.com>
2021-06-18 11:54:42 -0300

    Exercicio utilizando objetos

---
+++ Exercicio03.js
@@ -0,0 +1,8 @@
+// Programador: Victor Hugo | Chama os objetos de um string | data: 18/06/2021
+
+var name = {
+    first: 'Victor ',
+    middle: 'Hugo ',
+    last: 'Ferreira '
+}
+console.log(name.first + name.middle + name.last);
