a3ca24a10f9382e5f58123219d3d1e4e131c3ebb
huguds <vfvitinhoblack@gmail.com>
2021-06-19 23:51:46 -0300

    Exercicio que armazena a quantidade de caracteres utilizados

---
+++ Exercicio04.js
@@ -0,0 +1,5 @@
+// Programador: Victor Hugo | Armazena a quantidade de caracteres em uma String utilizando o camando length| data: 19/06/2021
+alimento = ['arroz','feijao','batata'];
+for (var i = 0; i < alimento.length; i++){
+    console.log ("Alimento "+i+"°: "+alimento[i]+" Possui "+alimento[i].length+"° quantidade de string");
+}
