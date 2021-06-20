a3adad453252d57af38af20a5e90460b123e34c5
huguds <vfvitinhoblack@gmail.com>
2021-06-20 10:58:20 -0300

    Operador ternário

---
+++ Exercicio05.js
@@ -0,0 +1,6 @@
+// Programador: Victor Hugo | Operador ternário | data: 20/06/2021
+var PrecoSedan = 1000;
+var PrecoSUV = 1500;
+// Operador ternário substitui a função do IF otimizando a linha de código
+PrecoSedan < PrecoSUV ? console.log("O preço do carro sedan está mais em conta !"):
+console.log("O preço do SUV está mais em conta !");
