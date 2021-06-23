c2735900f587f089a5b211485ec19444815fa71b
huguds <vfvitinhoblack@gmail.com>
2021-06-23 15:46:28 -0300

    Exercicio de manipulação de Strings e Arrays

--- Exercicio04.js
+++ Exercicio04.js
@@ -1,10 +1,14 @@
-// Programador: Victor Hugo | Manipulações de String | data: 19/06/2021
+// Programador: Victor Hugo | Manipulações de String e Arrays | data: 19/06/2021
 // 1 - Mostra os alimentos e calcula quantos caracteres cada um possui através do comando .length()
+console.log("\n");
+console.log("------------Exemplo 1------------")
 alimento = ['arroz','feijao','batata'];
 for (var i = 0; i < alimento.length; i++){
     console.log ("Alimento "+i+"°: "+alimento[i]+" Possui "+alimento[i].length+"° quantidade de string");
 }
-// 2 - Faz uma consulta e verifica se o parâmetro se encontra na variavél através do comando .includes()
+// 2 - Faz uma consulta e verifica se o parâmetro se encontra na variável através do comando .includes()
+console.log("\n");
+console.log("------------Exemplo 2------------")
 nome = 'Asdrubal';
 if (nome.includes('x')){
     console.log('Verdadeiro');
@@ -12,6 +16,8 @@ if (nome.includes('x')){
     console.log('Falso');
 }
 // 3 - Faz uma consulta e se encontrar o valor passado no parâmetro ele o substituirá através do comando .replace()
+console.log("\n");
+console.log("------------Exemplo 3------------")
 if (nome == 'Asdrubal'){
     nome = nome.replace('Asdrubal', 'Emengarda')
     console.log(nome);
@@ -19,8 +25,21 @@ if (nome == 'Asdrubal'){
     console.log('Falso');
 }
 // 4 - Retorna uma cópia de elementos do array original de acordo com os parâmetros através do comando .slice()
+console.log("\n");
+console.log("------------Exemplo 4------------")
 var fruits = ['Apple','Orange','BlueBarry']
 var copy = fruits.slice(0,3);
 for (let num of copy){
     console.log(num)
 };
+// 5 - Método adiciona um ou mais elementos ao final de um array e retorna o novo comprimento através do comando .push()
+console.log("\n");
+console.log("------------Exemplo 5------------")
+var transport = ['Bus','Taxi','Car'];
+transport.push('Limousine');
+console.log(transport.join(", ")); // O comando join é responsável por juntar o array da forma que estiver no parâmetro: ", "|' '|'-'|
+// 6 - Método remove o ultimo item de um array através do comando .pop()
+console.log("\n");
+console.log("------------Exemplo 6------------")
+transport.pop();
+console.log(transport.join(", "));
