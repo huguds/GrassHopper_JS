e1b993d249dedf7c651aee35738f07465e32b5e9
huguds <vfvitinhoblack@gmail.com>
2021-06-27 22:51:03 -0300

    Laço de repetição For

--- Exercicio01.js
+++ Exercicio01.js
@@ -1,22 +1,28 @@
 // Programador: Victor Hugo | Algoritmo: Laços de repetição | JavaScript Data: 17/06/2021
-/*
-// Laço de string
+// 1 - Laço de string
+console.log("\n");
+console.log("------------Exemplo 1------------");
 for (var letter of 'grasshopper'){
     console.log(letter);
 }
-// Contando com laços
+// 2 - Contando com laços
+console.log("\n");
+console.log("------------Exemplo 2------------");
 for (var i = 0; i < 10; i = i + 1){
     console.log(i);
 }
-// Contagem regressiva
+// 3 - Contagem regressiva utilizando o For
+console.log("\n");
+console.log("------------Exemplo 3------------");
 for (var i = 10; i > 0; i = i - 1){
     console.log(i);
 }
 console.log("Happy new year !");
-// Sugestões de nomes de banda
+// 4 - Manipulando arrays utilizando o For
+console.log("\n");
+console.log("------------Exemplo 4------------");
 for (var adj of ['Feliz','Triste','Alegre']){
     for (var subs of ['Victor','Hugo','Silva']){
         console.log('The ' + subs + ' ' + adj);
     }
 }
-*/
