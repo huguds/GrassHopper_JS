a8b7563485bcf784f7f98e7298a1163a9a34798e
huguds <vfvitinhoblack@gmail.com>
2021-06-27 22:55:08 -0300

    Estrutura de decisão

--- Exercicio02.js
+++ Exercicio02.js
@@ -1,5 +1,7 @@
 // Programador: Victor Hugo | Algoritmo: estrutura de decisão | JavaScript Data: 17/06/2021
-
+// 1 - Estrutura de decisão utilizando if
+console.log("\n");
+console.log("------------Exemplo 1------------");
 var nota1 = 0;
 var nota2 = 10;
 var media = (nota1+nota2)/2;
