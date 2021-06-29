37cfc7bc72256f9ec34a1eff87888220c6afc089
huguds <vfvitinhoblack@gmail.com>
2021-06-29 20:44:48 -0300

    Dicas e boas práticas ao programar em JS

--- Exercicio06.js
+++ Exercicio06.js
@@ -1,15 +1,15 @@
 // Programador: Victor Hugo | Funções | data: 21/06/2021
 // Uma função é um conjunto de instruções que retornam um valor quando chamado.
+console.log("---------Exemplo 1---------");
 function Dollars(Real){ // Função para converter Real em dólares
     let Result = Real*5.04;
     return Result;
 };
-//console.log('100 Reais = '+Dollars(100)+' Dollars');
-
-// Função para retornar o maior numero de um array
-
+console.log('100 Reais = '+Dollars(100)+' Dollars');
+// 2 - Função para retornar o maior numero de um array
+console.log("\n");
+console.log("---------Exemplo 2---------");
 var list = [58,25,99,57,64];
-
 function larger(a,b){ // Função retorna o maior número
     return a > b ? a : b;
 };
